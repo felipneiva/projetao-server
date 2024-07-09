1377c62a372993615a145613f089075ff6a1d522
Felipe Neiva <felipeneivals@gmail.com>
2024-07-09 11:25:47 -0300

    feat(users): add user creation endpoint

--- src/api/router.py
+++ src/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 from src.api import test
+from src.api import user
 
 
 api_router = APIRouter()
 api_router.include_router(test.router, prefix="/test", tags=["test"])
-
+api_router.include_router(user.router, prefix="/users", tags=["users"])
