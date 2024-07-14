46a2f0d67b1c195db2b5f5a95d2475d4e4888dc9
Felipe Neiva <felipeneivals@gmail.com>
2024-07-14 20:46:25 -0300

    feat(daily reports): add daily reports endpoint to router

--- src/api/router.py
+++ src/api/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from src.api import test
 from src.api import user
+from src.api import daily_reports
 
 
 api_router = APIRouter()
 api_router.include_router(test.router, prefix="/test", tags=["test"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(daily_reports.router, prefix="/daily_reports", tags=["daily_reports"])
