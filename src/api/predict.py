b13ce0447328d2d40fc49dc5b26127cac18fb7ae
Felipe Neiva <felipeneivals@gmail.com>
2024-07-28 16:05:10 -0300

    chore: remove useless comments

--- src/api/predict.py
+++ src/api/predict.py
@@ -5,15 +5,12 @@ import pandas as pd
 import xgboost as xgb
 import os
 
-# Dynamically determine the model path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(current_dir, '..', 'model', 'overall_model.json')
 
-# Load the model (make sure the model file path is correct)
 loaded_model = xgb.XGBClassifier()
 loaded_model.load_model(model_path)
 
-# Method to perform prediction given an input
 def predict(input_data, model):
     """
     Predict if the user is smoking given input data.
@@ -29,15 +26,6 @@ def predict(input_data, model):
     input_df = pd.DataFrame([input_data], columns=['latitude', 'longitude', 'seconds', 'minutes', 'hour', 'day_of_week'])
     prediction = model.predict(input_df)
     return int(prediction[0])
-
-# Define the request body model
-class PredictionInput(BaseModel):
-    latitude: float
-    longitude: float
-    seconds: int
-    minutes: int
-    hour: int
-    day_of_week: int
 
 router = APIRouter()
 
